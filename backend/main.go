2ac62c353f0edd78887028f1f93669697621ba8b
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-03-26 22:43:59 +0200

    completed main

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"backend/api"
+	"backend/initializers"
 
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+    initializers.LoadEnvVariables()
+    initializers.ConnectToDb()
+    initializers.SyncDatabase()
+}
+
+func main() {
+    r:=gin.Default()
+    api.Routes(r)
+    r.Run()
 }
