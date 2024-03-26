f2da2d8be13ac4ff692e90fb8c07de030b745c1a
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-03-26 20:45:04 +0200

    update

--- frontend/src/data/portfolio.js
+++ frontend/src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
    {
-      title: 'Terminal Website',
-      imgUrl: '/assets/terminal-website.jpg',
-      stack: ['HTML', 'CSS', 'JavaScript'],
+      title: 'Project 1',
+      imgUrl: '/assets/coming-soon.jpg',
+      stack: ['TBC', 'TBC', 'TBC'],
       link: 'https://fkcodes.com',
    },
    {
-      title: 'Developer Portfolio',
-      imgUrl: '/assets/dev-portfolio.jpg',
-      stack: ['React', 'Tailwind', 'Vite'],
+      title: 'Project 2',
+      imgUrl: '/assets/coming-soon.jpg',
+      stack: ['TBC', 'TBC', 'TBC'],
       link: 'https://thehackershideout.com',
    },
    {
-      title: 'AI Nerf Gun',
-      imgUrl: '/assets/ai-nerf-gun.jpg',
-      stack: ['Python', 'C++', 'Robotics'],
+      title: 'Project 3',
+      imgUrl: '/assets/coming-soon.jpg',
+      stack: ['TBC', 'TBC', 'TBC'],
       link: 'https://youtu.be/A8eegxL82PI',
    },
 ]
