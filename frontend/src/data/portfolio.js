d78ee549ccf1c9889f84c11212ac55ea28333f77
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-10-19 19:46:09 +0200

    add reastaurnant and reoder projects

--- frontend/src/data/portfolio.js
+++ frontend/src/data/portfolio.js
@@ -1,45 +1,52 @@
 export default [
-   {
-      title: 'Pharmacy Web Page',
-      imgUrl: '/assets/pharmacy.png',
-      stack: ['REACT', 'NODE', 'MONGO','REDIS'],
-      link: 'https://pharmacy-web-page-g8v8.vercel.app/',
-   },
-   {
-      title: 'Ecommerce Shopping Cart',
-      imgUrl: '/assets/ecommerce.png',
-      stack: ['TS', 'GO', 'TAILWIND'],
-      link: 'https://github.com/CovetingAphid3/ecommerce-shopping-cart',
-   },
-   {
-      title: 'Admin Dashboard',
-      imgUrl: '/assets/admin-dashboard.png',
-      stack: ['VUE','GO' ,'TAILWIND','TS'],
-      link: 'https://github.com/Covetingaphid3/admin-dashboard',
-   },
-   {
-      title: 'Chat Application',
-      imgUrl: '/assets/chat.png',
-      stack: ['GO', 'SOCKET', 'JS'],
-      link: 'https://github.com/Covetingaphid3/Chat-Application',
-   },
-   {
-      title: 'Data Structure & Algorithms',
-      imgUrl: '/assets/dsa.webp',
-      stack: ['PY', 'TS', 'CS',"GO"],
-      link: 'https://github.com/Covetingaphid3/dsa',
-   },
-   {
-      title: 'Online Laundromat',
-      imgUrl: '/assets/laundry.png',
-      stack: ['PY', 'FLASK', 'BS'],
-      link: 'https://github.com/Covetingaphid3/laundromat',
-   },
-   {
-      title: 'Inventory Management System',
-      imgUrl: '/assets/inventory.png',
-      stack: ['REACT', 'TS', 'MONGO'],
-      link: 'https://github.com/Covetingaphid3/Inventory-Management',
-   },
+    {
+        title: 'Pharmacy Web Page',
+        imgUrl: '/assets/pharmacy.png',
+        stack: ['REACT', 'NODE', 'MONGO', 'REDIS'],
+        link: 'https://pharmacy-web-page-g8v8.vercel.app/',
+    },
+    {
+        title: 'Ecommerce Shopping Cart',
+        imgUrl: '/assets/ecommerce.png',
+        stack: ['TS', 'GO', 'TAILWIND'],
+        link: 'https://github.com/CovetingAphid3/ecommerce-shopping-cart',
+    },
+    {
+        title: 'Admin Dashboard',
+        imgUrl: '/assets/admin-dashboard.png',
+        stack: ['VUE', 'GO', 'TAILWIND', 'TS'],
+        link: 'https://github.com/Covetingaphid3/admin-dashboard',
+    },
+
+    {
+        title: 'Restaurant Web App',
+        imgUrl: '/assets/restaurant.png',
+        stack: ['VUE', 'TAILWIND', 'TS'],
+        link: 'https://restaurant-prototype.vercel.app',
+    },
+    {
+        title: 'Chat Application',
+        imgUrl: '/assets/chat.png',
+        stack: ['GO', 'SOCKET', 'JS'],
+        link: 'https://github.com/Covetingaphid3/Chat-Application',
+    },
+    {
+        title: 'Online Laundromat',
+        imgUrl: '/assets/laundry.png',
+        stack: ['PY', 'FLASK', 'BS'],
+        link: 'https://github.com/Covetingaphid3/laundromat',
+    },
+    {
+        title: 'Inventory Management System',
+        imgUrl: '/assets/inventory.png',
+        stack: ['REACT', 'TS', 'MONGO'],
+        link: 'https://github.com/Covetingaphid3/Inventory-Management',
+    },
+    {
+        title: 'Data Structure & Algorithms',
+        imgUrl: '/assets/dsa.webp',
+        stack: ['PY', 'TS', 'CS', "GO"],
+        link: 'https://github.com/Covetingaphid3/dsa',
+    },
 ];
 
