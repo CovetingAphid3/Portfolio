0c238f0924fbe9e1de9d0cfaf06910e5028c921d
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-10-27 12:14:13 +0200

    remove unused images

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,18 +1,8 @@
 import {
-  nft,
-  people01,
-  people02,
-  people03,
   facebook,
   instagram,
   linkedin,
   twitter,
-  binance,
-  coinbase,
-  dropbox,
-  send,
-  shield,
-  star,
 } from "../assets";
 
 export const navLinks = [
@@ -34,74 +24,6 @@ export const navLinks = [
   },
 ];
 
-export const features = [
-  {
-    id: "feature-1",
-    icon: star,
-    title: "Market Rewards",
-    content:
-      "Unlock exclusive rewards and benefits with our unparalleled trading solutions.",
-  },
-  {
-    id: "feature-2",
-    icon: shield,
-    title: "Secure Transactions",
-    content:
-      "We prioritize the security of your financial transactions, ensuring a 100% secure environment.",
-  },
-  {
-    id: "feature-3",
-    icon: send,
-    title: "Global Trading",
-    content:
-      "Expand your horizons with our global trading platform, facilitating seamless transactions worldwide.",
-  },
-];
-
-export const feedback = [
-  {
-    id: "feedback-1",
-    content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the trader.",
-    name: "Herman Jensen",
-    title: "Founder & Trader",
-    img: people01,
-  },
-  {
-    id: "feedback-2",
-    content:
-      "Money makes your trading journey easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
-    title: "Founder & Trader",
-    img: people02,
-  },
-  {
-    id: "feedback-3",
-    content:
-      "It is usually people in the money business, finance, and international trade that are really successful.",
-    name: "Kenn Gallagher",
-    title: "Founder & Trader",
-    img: people03,
-  },
-];
-
-export const stats = [
-  {
-    id: "stats-1",
-    title: "Traders Active",
-    value: "3800+",
-  },
-  {
-    id: "stats-2",
-    title: "Trusted by Traders",
-    value: "230+",
-  },
-  {
-    id: "stats-3",
-    title: "Total Transactions",
-    value: "$230M+",
-  },
-];
 export const footerLinks = [
   {
     title: "Useful Links",
@@ -153,21 +75,3 @@ export const socialMedia = [
   },
 ];
 
-export const clients = [
-  {
-    id: "client-1",
-    logo: "",
-  },
-  {
-    id: "client-2",
-    logo: binance,
-  },
-  {
-    id: "client-3",
-    logo: nft,
-  },
-  {
-    id: "client-4",
-    logo: coinbase,
-  },
-];
